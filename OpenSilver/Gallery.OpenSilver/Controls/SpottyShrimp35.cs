23308c437675b3149ffc6961b3f0c5f0091a4ccd
vincent <christian289@naver.com>
2025-12-10 19:26:40 +0900

    OpenSilver 1차 커밋

---
+++ OpenSilver/Gallery.OpenSilver/Controls/SpottyShrimp35.cs
@@ -0,0 +1,17 @@
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Gallery.OpenSilver.Controls
+{
+    /// <summary>
+    /// CSS repeating-linear-gradient 패턴을 구현한 컨트롤
+    /// Control implementing CSS repeating-linear-gradient pattern
+    /// </summary>
+    public class SpottyShrimp35 : Control
+    {
+        public SpottyShrimp35()
+        {
+            DefaultStyleKey = typeof(SpottyShrimp35);
+        }
+    }
+}
